eda743db31b4324a6a1d38da2591d0feae17bffd
Simon Neutert <simonneutert@users.noreply.github.com>
2023-05-07 11:03:35 +0200

    meta and dropping bootstrap cdn

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import Nav from '../components/nav'
-import Footer from '../components/footer'
+import { Html, Head, Main, NextScript } from "next/document";
+import Nav from "../components/nav";
+import Footer from "../components/footer";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="de">
       <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" defer />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous" defer></script>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="/bootstrap.min.css" rel="stylesheet" defer />
+        <script src="/bootstrap.bundle.min.js" defer></script>
       </Head>
 
       <body>
-
         <Nav />
 
         <div class="container my-4">
@@ -24,5 +24,5 @@ export default function Document() {
         <Footer />
       </body>
     </Html>
-  )
+  );
 }
